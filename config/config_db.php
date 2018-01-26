07885e2f51d653b76bbf4a9dc6d1b18d06a8ac61
zm <oneyii@163.com>
2018-01-26 15:32:05 +0800

    提交

--- config/config_db.php
+++ config/config_db.php
@@ -1,8 +1,8 @@
 <?php
                    /*
-                   con_db_host = "sztcmdiet.com"
+                   con_db_host = "xxx"
                    con_db_id   = "root"
-                   con_db_pass	= "djk@Red13health"
+                   con_db_pass	= "xxxx"
                    con_db_name = "met"
                    tablepre    =  "met_"
                    db_charset  =  "utf8";
